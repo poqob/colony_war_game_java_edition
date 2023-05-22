fff1c845415de5010fe214cad3cb5064150c77de
Mustafa <mmermmus123@gmail.com>
2023-05-22 16:14:08 +0300

    Ui done

---
+++ colony_war_game_java_edition/src/ui/Ui.java
@@ -0,0 +1,29 @@
+package ui;
+
+import java.util.ArrayList;
+
+import log.Log;
+
+public class Ui {
+	private static String divider = "\n-------------------------------------------------------";
+
+	public static void showLogs(ArrayList<ArrayList<Log>> listOfLogsList) {
+		String res = "";
+		ArrayList<Log> logs;
+		Log log;
+
+		for (int i = 0; i < listOfLogsList.size(); i++) {
+			logs = listOfLogsList.get(i);
+			res += divider;
+			res += "\nTour: " + (i + 1);
+			res += "\nColony    Population      Food Stock      Victory Loose";
+
+			for (int j = 0; j < logs.size(); j++) {
+				log = logs.get(j);
+				res += "\n " + log.toString();
+			}
+		}
+		res += divider + "\n";
+		System.out.print(res);
+	}
+}
