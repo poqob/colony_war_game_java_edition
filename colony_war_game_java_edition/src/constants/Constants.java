8ed086b31fa9e1769b6b23d70f265188edb112a8
Mustafa <mmermmus123@gmail.com>
2023-05-22 15:23:52 +0300

    Todo annotation and Constants of program added to project

--- colony_war_game_java_edition/src/constants/Constants.java
+++ colony_war_game_java_edition/src/constants/Constants.java
@@ -1,6 +1,9 @@
 package constants;
 
+import java.util.ArrayList;
+
 public class Constants {
 	public static int INDEX = 100;
 	public static int POPULATION_SCALE_AS_PERCENTAGE = 120;
+	public static ArrayList<String> symbols;
 }
---
+++ colony_war_game_java_edition/src/utils/Todo.java
@@ -0,0 +1,16 @@
+/**
+*
+* @author Mustafa BICER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 07.04.23
+* @Todo
+* i prefer to use todo("something") where i code. i added but i've learned after. 
+* java already has it.
+* 
+*/
+
+package utils;
+
+public @interface Todo {
+	String value();
+
+}
